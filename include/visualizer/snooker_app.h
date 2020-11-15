4828b45486af5247ead2726e47c678b8afe86d68
rtpatel2 <rtpatel2@illinois.edu>
2020-11-15 00:44:10 -0500

    Created constants to define snooker table parameters.

--- include/visualizer/snooker_app.h
+++ include/visualizer/snooker_app.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include "core/table.h"
 #include "cinder/gl/gl.h"
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
@@ -23,6 +24,28 @@ class SnookerApp : public ci::app::App {
 
   /** Renders the current state of the snooker visualization. */
   void draw() override;
+
+  /** Scaling factor for this visualization. */
+  const float kScalingFactor = 2.5f;
+
+  /** Size of the margins along the edges of the window. */
+  const float kHorizontalMargin = 100.0f;
+  const float kVerticalMargin = 100.0f;
+
+  /** Dimensions of the table. */
+  const float kTableWidth = 356.9f * kScalingFactor;
+  const float kTableHeight = 177.8f * kScalingFactor;
+
+  /** Dimensions of the window -- margins on each side, plus central area. */
+  const float kWindowWidth = 2 * kHorizontalMargin + kTableWidth;
+  const float kWindowHeight = 2 * kVerticalMargin + kTableHeight;
+
+ private:
+  const ci::Color kWhite = ci::Color("white");
+  const ci::Color kBlack = ci::Color("black");
+  const ci::Color kRed = ci::Color("red");
+
+  Table table_;
 };
 
 }  // namespace visualizer
--- src/visualizer/snooker_app.cc
+++ src/visualizer/snooker_app.cc
@@ -8,6 +8,13 @@ namespace snooker {
 
 namespace visualizer {
 
+SnookerApp::SnookerApp() : table_(ci::Rectf(kHorizontalMargin, kVerticalMargin,
+                                          kHorizontalMargin + kTableWidth,
+                                          kVerticalMargin + kTableHeight)) {
+  ci::app::setWindowSize(static_cast<int>(kWindowWidth),
+                         static_cast<int>(kWindowHeight));
+}
+
 
 }  // namespace visualizer
 
