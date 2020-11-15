8f106e6b1d9e0690cd9f8e550ac3bc93ef0183e4
rtpatel2 <rtpatel2@illinois.edu>
2020-11-14 22:43:22 -0500

    Implemented constructor for Ball class.

--- include/core/ball.h
+++ include/core/ball.h
@@ -3,3 +3,33 @@
  */
 
 #pragma once
+
+#include "cinder/gl/gl.h"
+
+namespace snooker {
+
+/** Maintains functionality of a snooker Ball. */
+class Ball {
+ public:
+  /**
+   * Creates a new snooker Ball with the specified position, velocity, color,
+   * mass, and radius.
+   *
+   * @param initial_position initial position of the Ball.
+   * @param initial_velocity initial velocity of the Ball.
+   * @param color color of the Ball.
+   * @param mass mass of the Ball.
+   * @param radius radius of the Ball.
+   */
+  Ball(const glm::vec2& initial_position, const glm::vec2& initial_velocity,
+       const ci::Color& color, float mass, float radius);
+
+ public:
+  glm::vec2 position_;
+  glm::vec2 velocity_;
+  ci::Color color_;
+  float mass_;
+  float radius_;
+};
+
+}  // namespace snooker
--- src/core/ball.cc
+++ src/core/ball.cc
@@ -1,3 +1,18 @@
 /**
  * Created by Rishi Patel on 11/14/2020.
  */
+
+#include "core/ball.h"
+#include "cinder/gl/gl.h"
+
+namespace snooker {
+
+Ball::Ball(const glm::vec2& initial_position, const glm::vec2& initial_velocity,
+           const ci::Color& color, float mass, float radius)
+    : position_(initial_position),
+      velocity_(initial_velocity),
+      color_(color),
+      mass_(mass),
+      radius_(radius) {}
+
+}  // namespace snooker
