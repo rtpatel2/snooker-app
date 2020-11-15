60e38121eee068af49dcdbd0cf2deda84884d847
rtpatel2 <rtpatel2@illinois.edu>
2020-11-15 00:38:37 -0500

    Wrote framework for functionality of SnookerApp.

--- include/visualizer/snooker_app.h
+++ include/visualizer/snooker_app.h
@@ -3,3 +3,28 @@
  */
 
 #pragma once
+
+#include "cinder/gl/gl.h"
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+
+namespace snooker {
+
+namespace visualizer {
+
+/** Allows a user to visualize a game of snooker. */
+class SnookerApp : public ci::app::App {
+ public:
+  /** Initializes a new SnookerApp. */
+  SnookerApp();
+
+  /** Performs once per loop computations to be used in draw(). */
+  void update() override;
+
+  /** Renders the current state of the snooker visualization. */
+  void draw() override;
+};
+
+}  // namespace visualizer
+
+}  // namespace snooker
--- src/visualizer/snooker_app.cc
+++ src/visualizer/snooker_app.cc
@@ -1,3 +1,14 @@
 /**
  * Created by Rishi Patel on 11/14/2020.
  */
+
+#include "visualizer/snooker_app.h"
+
+namespace snooker {
+
+namespace visualizer {
+
+
+}  // namespace visualizer
+
+}  // namespace snooker
