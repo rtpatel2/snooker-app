7e77ded30d8ea1f72d3816c0c9cceb11e942f719
rtpatel2 <rtpatel2@illinois.edu>
2020-11-15 00:28:56 -0500

    Implemented constructor for Table class.

--- include/core/table.h
+++ include/core/table.h
@@ -4,11 +4,22 @@
 
 #pragma once
 
+#include "cinder/gl/gl.h"
+
 namespace snooker {
 
 /** Maintains functionality of a snooker table. */
 class Table {
+ public:
+  /**
+   * Creates a new Table with the specified rectangular walls.
+   *
+   * @param walls Cinder rectangle specifying the walls of the Table.
+   */
+  Table(const ci::Rectf& walls);
 
+ private:
+  ci::Rectf walls_;
 };
 
 }  // namespace snooker
--- src/core/table.cc
+++ src/core/table.cc
@@ -2,7 +2,11 @@
  * Created by Rishi Patel on 11/15/2020.
  */
 
+#include "core/table.h"
+#include "cinder/gl/gl.h"
+
 namespace snooker {
 
+Table::Table(const ci::Rectf& walls) : walls_(walls) {}
 
 }  // namespace snooker
