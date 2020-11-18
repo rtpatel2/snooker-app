cd75cf412c328ef4d6657cd1172ed05f7f14c2b0
rtpatel2 <rtpatel2@illinois.edu>
2020-11-17 23:44:13 -0500

    Implemented tests to validate constructors for Table class.

--- tests/table_test.cc
+++ tests/table_test.cc
@@ -4,11 +4,14 @@
 
 #include "core/table.h"
 #include "core/ball.h"
+#include "core/table_cushion.h"
 #include "cinder/gl/gl.h"
 #include "catch2/catch.hpp"
 
-using snooker::Table;
 using snooker::Ball;
+using snooker::Table;
+using snooker::TableCushion;
+using snooker::StraightCushion;
 
 /**
  * Testing strategy: test each function in the Ball class, including all
@@ -18,11 +21,18 @@ using snooker::Ball;
 static const double kMarginOfError = 0.001;
 
 TEST_CASE("Creating a Table.") {
+  // Sanity check -- nothing to check via require statements.
   SECTION("Calling default constructor.") {
-
+    Table table;
   }
 
   SECTION("Calling overloaded constructor.") {
+    StraightCushion cushion1(ci::Rectf(100, 110, 115, 300));
+    std::vector<TableCushion*> cushions{&cushion1};
+    Table table(ci::Rectf(100, 100, 500, 500), cushions);
 
+    REQUIRE(glm::vec2(100, 100) == table.GetWalls().getUpperLeft());
+    REQUIRE(glm::vec2(500, 500) == table.GetWalls().getLowerRight());
+    REQUIRE(1 == table.GetCushions().size());
   }
 }
