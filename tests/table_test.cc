f866520d0925c529242f864137580a2fe5db621a
rtpatel2 <rtpatel2@illinois.edu>
2020-11-17 23:48:52 -0500

    Implemented tests to validate adding Balls to a Table.

--- tests/table_test.cc
+++ tests/table_test.cc
@@ -36,3 +36,35 @@ TEST_CASE("Creating a Table.") {
     REQUIRE(1 == table.GetCushions().size());
   }
 }
+
+TEST_CASE("Validate adding Balls to the Table.") {
+  StraightCushion cushion1(ci::Rectf(100, 110, 115, 300));
+  std::vector<TableCushion*> cushions{&cushion1};
+  Table table(ci::Rectf(100, 100, 500, 500), cushions);
+
+  SECTION("Adding one Ball to an empty Table.") {
+    table.AddBall(
+        Ball(glm::vec2(300, 300), glm::vec2(60, 80), ci::Color("blue"), 1, 2));
+    REQUIRE(1 == table.GetBalls().size());
+    REQUIRE(glm::vec2(300, 300) == table.GetBalls()[0].GetPosition());
+  }
+
+  SECTION("Adding multiple Balls to a Table.") {
+    table.AddBall(
+        Ball(glm::vec2(300, 300), glm::vec2(60, 80), ci::Color("blue"), 1, 2));
+    table.AddBall(
+        Ball(glm::vec2(400, 300), glm::vec2(-4, 6), ci::Color("green"), 4, 5));
+    REQUIRE(2 == table.GetBalls().size());
+    REQUIRE(glm::vec2(300, 300) == table.GetBalls()[0].GetPosition());
+    REQUIRE(glm::vec2(-0.04, 0.06) == table.GetBalls()[1].GetVelocity());
+  }
+
+  SECTION("Attempting to add Balls outside of the walls of a Table.") {
+    table.AddBall(
+        Ball(glm::vec2(300, 300), glm::vec2(60, 80), ci::Color("blue"), 1, 2));
+    table.AddBall(
+        Ball(glm::vec2(40, 300), glm::vec2(-4, 6), ci::Color("green"), 4, 5));
+    REQUIRE(1 == table.GetBalls().size());
+    REQUIRE(glm::vec2(300, 300) == table.GetBalls()[0].GetPosition());
+  }
+}
