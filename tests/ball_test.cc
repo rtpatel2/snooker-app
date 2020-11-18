7279dc1434ab51b856e9e290dd685a3a2220101f
rtpatel2 <rtpatel2@illinois.edu>
2020-11-17 23:05:34 -0500

    Implemented test to validate Ball constructor.

--- tests/ball_test.cc
+++ tests/ball_test.cc
@@ -1,3 +1,28 @@
 /**
  * Created by Rishi Patel on 11/14/2020.
  */
+
+#include "core/ball.h"
+#include "cinder/gl/gl.h"
+#include "catch2/catch.hpp"
+
+using snooker::Ball;
+
+/**
+ * Testing strategy: test each function in the Ball class, including all
+ * relevant edge cases.
+ */
+
+static const double kMarginOfError = 0.001;
+
+TEST_CASE("Creating a Ball.") {
+  SECTION("Calling the constructor.") {
+    Ball ball(glm::vec2(300, 400), glm::vec2(-1.2, 3.4), ci::Color("red"),
+              12.3f, 4.3f);
+    REQUIRE(glm::vec2(300, 400) == ball.GetPosition());
+    REQUIRE(glm::vec2(-1.2, 3.4) == ball.GetVelocity());
+    REQUIRE(ci::Color("red") == ball.GetColor());
+    REQUIRE(12.3 == ball.GetRadius());
+    REQUIRE(4.3 == ball.GetMass());
+  }
+}
