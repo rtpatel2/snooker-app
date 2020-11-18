0ad364fc76aa5444e97f1b735d43ca8b5c896c21
rtpatel2 <rtpatel2@illinois.edu>
2020-11-17 23:38:15 -0500

    Created testing file for Table class.

---
+++ tests/table_test.cc
@@ -0,0 +1,28 @@
+/**
+ * Created by Rishi Patel on 11/17/2020.
+ */
+
+#include "core/table.h"
+#include "core/ball.h"
+#include "cinder/gl/gl.h"
+#include "catch2/catch.hpp"
+
+using snooker::Table;
+using snooker::Ball;
+
+/**
+ * Testing strategy: test each function in the Ball class, including all
+ * relevant edge cases.
+ */
+
+static const double kMarginOfError = 0.001;
+
+TEST_CASE("Creating a Table.") {
+  SECTION("Calling default constructor.") {
+
+  }
+
+  SECTION("Calling overloaded constructor.") {
+
+  }
+}
