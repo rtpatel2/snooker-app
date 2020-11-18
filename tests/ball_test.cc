4f4fb0c22d17692b0681d4f75fe297bc4c915cef
rtpatel2 <rtpatel2@illinois.edu>
2020-11-17 23:28:03 -0500

    Implemented tests to validate updating a Ball's position.

--- tests/ball_test.cc
+++ tests/ball_test.cc
@@ -20,9 +20,53 @@ TEST_CASE("Creating a Ball.") {
     Ball ball(glm::vec2(300, 400), glm::vec2(-1.2, 3.4), ci::Color("red"),
               12.3f, 4.3f);
     REQUIRE(glm::vec2(300, 400) == ball.GetPosition());
-    REQUIRE(glm::vec2(-1.2, 3.4) == ball.GetVelocity());
+    REQUIRE(glm::vec2(-0.012, 0.034) == ball.GetVelocity());
     REQUIRE(ci::Color("red") == ball.GetColor());
-    REQUIRE(12.3 == ball.GetRadius());
-    REQUIRE(4.3 == ball.GetMass());
+    REQUIRE(12.3f == ball.GetRadius());
+    REQUIRE(4.3f == ball.GetMass());
   }
+}
+
+TEST_CASE("Validate updating a Ball's position.") {
+  SECTION("Ball with non-zero velocity components.") {
+    Ball ball(glm::vec2(100, 100), glm::vec2(40, 30), ci::Color("white"), 1, 1);
+    ball.UpdatePosition();
+    REQUIRE(0.3608 == Approx(ball.GetVelocity().x).margin(kMarginOfError));
+    REQUIRE(0.2706 == Approx(ball.GetVelocity().y).margin(kMarginOfError));
+    REQUIRE(100.3608 == Approx(ball.GetPosition().x).margin(kMarginOfError));
+    REQUIRE(100.2706 == Approx(ball.GetPosition().y).margin(kMarginOfError));
+  }
+
+  SECTION("Ball with one non-zero velocity component.") {
+    Ball ball(glm::vec2(100, 100), glm::vec2(0, -300), ci::Color("white"), 1,
+              1);
+    ball.UpdatePosition();
+    REQUIRE(0 == Approx(ball.GetVelocity().x).margin(kMarginOfError));
+    REQUIRE(-2.951 == Approx(ball.GetVelocity().y).margin(kMarginOfError));
+    REQUIRE(100 == Approx(ball.GetPosition().x).margin(kMarginOfError));
+    REQUIRE(97.049 == Approx(ball.GetPosition().y).margin(kMarginOfError));
+  }
+
+  SECTION("Ball with zero speed.") {
+    Ball ball(glm::vec2(345, 677), glm::vec2(0, 0), ci::Color("white"), 1, 1);
+    ball.UpdatePosition();
+    REQUIRE(0 == Approx(ball.GetVelocity().x).margin(kMarginOfError));
+    REQUIRE(0 == Approx(ball.GetVelocity().y).margin(kMarginOfError));
+    REQUIRE(345 == Approx(ball.GetPosition().x).margin(kMarginOfError));
+    REQUIRE(677 == Approx(ball.GetPosition().y).margin(kMarginOfError));
+  }
+
+  SECTION("Ball coming to a stop due to friction.") {
+    Ball ball(glm::vec2(300, 400), glm::vec2(-1.2, 3.4), ci::Color("red"),
+              12.3f, 4.3f);
+    ball.UpdatePosition();
+    REQUIRE(0 == Approx(ball.GetVelocity().x).margin(kMarginOfError));
+    REQUIRE(0 == Approx(ball.GetVelocity().y).margin(kMarginOfError));
+    REQUIRE(300 == Approx(ball.GetPosition().x).margin(kMarginOfError));
+    REQUIRE(400 == Approx(ball.GetPosition().y).margin(kMarginOfError));
+  }
+}
+
+TEST_CASE("Validate computing velocity after collision with another Ball.") {
+  SECTION("")
 }
