7b04606cbb535e5ab3f1e2b52e1c014a320a4bfd
rtpatel2 <rtpatel2@illinois.edu>
2020-11-29 15:32:16 -0500

    Wrote class to maintain functionality of snooker Pockets.

---
+++ include/core/pocket.h
@@ -0,0 +1,15 @@
+/**
+ * Created by Rishi Patel on 11/29/2020.
+ */
+
+#pragma once
+
+namespace snooker {
+
+/** Maintains functionality of a pocket on a snooker Table. */
+class Pocket {
+ public:
+
+};
+
+}  // namespace snooker
---
+++ src/core/pocket.cc
@@ -0,0 +1,9 @@
+//
+// Created by infernus on 11/29/2020.
+//
+
+#include "core/pocket.h"
+
+namespace snooker {
+
+}  // namespace snooker
