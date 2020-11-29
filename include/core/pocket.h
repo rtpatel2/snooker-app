b63ac8cd0dac075d57e924e2ceac52017c90f225
rtpatel2 <rtpatel2@illinois.edu>
2020-11-29 15:37:32 -0500

    Implemented Pocket class functionality to determine if a Ball will be pocketed.

--- include/core/pocket.h
+++ include/core/pocket.h
@@ -4,12 +4,35 @@
 
 #pragma once
 
+#include "ball.h"
+#include "cinder/gl/gl.h"
+
 namespace snooker {
 
 /** Maintains functionality of a pocket on a snooker Table. */
 class Pocket {
  public:
+  /**
+   * Creates a new Pocket for a snooker Table.
+   *
+   * @param position position of this Pocket.
+   * @param radius radius of this Pocket.
+   * @param color color of this Pocket.
+   */
+  Pocket(const glm::vec2& position, float radius, const ci::Color& color);
 
+  /**
+   * Determines if the given Ball will be pocketed in this Pocket.
+   *
+   * @param ball Ball to determine whether or not will be pocketed.
+   * @return true if the Ball will be pocketed, and false otherwise.
+   */
+  bool DetermineIfPocketed(const Ball& ball) const;
+
+ private:
+  glm::vec2 position_;
+  float radius_;
+  ci::Color color_;
 };
 
 }  // namespace snooker
--- src/core/pocket.cc
+++ src/core/pocket.cc
@@ -3,7 +3,20 @@
 //
 
 #include "core/pocket.h"
+#include "core/ball.h"
+#include "cinder/gl/gl.h"
 
 namespace snooker {
+
+Pocket::Pocket(const glm::vec2& position, float radius, const ci::Color& color)
+    : position_(position), radius_(radius), color_(color) {}
+
+bool Pocket::DetermineIfPocketed(const Ball& ball) const {
+  if (glm::distance(position_, ball.GetPosition()) <= radius_) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 }  // namespace snooker
