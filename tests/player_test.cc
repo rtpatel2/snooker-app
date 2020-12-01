049a92045c7863eb5b1290762ec9bf1483e1cb7d
rtpatel2 <rtpatel2@illinois.edu>
2020-12-01 17:56:16 -0500

    Implemented tests to validate determining if a stroke is legal or not.

--- tests/player_test.cc
+++ tests/player_test.cc
@@ -35,45 +35,99 @@ TEST_CASE("Validate determining if a stroke is legal.") {
   // The following sections apply to cases when the ball-on is red.
 
   SECTION("Stroke in which first contacted Ball is not red is illegal.") {
-    
+    table.SetCueBallFirstContacted(table.GetBalls()[1].GetColor());
+    table.SetCueBallFirstContacted(ci::Color("orange"));
+    REQUIRE_FALSE(player.IsStrokeLegal(table));
   }
 
   SECTION("Pocketing the cue Ball is illegal.") {
-
+    player.AddBallsPottedLastStroke(table.GetBalls().back());
+    REQUIRE_FALSE(player.IsStrokeLegal(table));
   }
 
   SECTION("Pocketing any non-red Ball is illegal.") {
-
+    table.SetCueBallFirstContacted(table.GetBalls()[20].GetColor());
+    player.AddBallsPottedLastStroke(table.GetBalls()[20]);
+    REQUIRE_FALSE(player.IsStrokeLegal(table));
   }
 
   SECTION("Pocketing one red Ball is legal.") {
-
+    table.SetCueBallFirstContacted(table.GetBalls()[1].GetColor());
+    player.AddBallsPottedLastStroke(table.GetBalls()[1]);
+    REQUIRE(player.IsStrokeLegal(table));
   }
 
   SECTION("Pocketing multiple red Balls is legal.") {
-
+    table.SetCueBallFirstContacted(table.GetBalls()[3].GetColor());
+    player.AddBallsPottedLastStroke(table.GetBalls()[3]);
+    player.AddBallsPottedLastStroke(table.GetBalls()[5]);
+    player.AddBallsPottedLastStroke(table.GetBalls()[7]);
+    REQUIRE(player.IsStrokeLegal(table));
   }
 
   // The following sections apply to cases when the ball-on is colored.
 
   SECTION("Pocketing multiple colors is illegal.") {
-
+    player.EndStroke(true);
+    table.SetCueBallFirstContacted(ci::Color("black"));
+    player.AddBallsPottedLastStroke(table.GetBalls()[20]);
+    player.AddBallsPottedLastStroke(table.GetBalls()[19]);
+    REQUIRE_FALSE(player.IsStrokeLegal(table));
   }
 
   SECTION("Pocketing a color different from the first contacted is illegal.") {
-
+    player.EndStroke(true);
+    table.SetCueBallFirstContacted(ci::Color("green"));
+    player.AddBallsPottedLastStroke(table.GetBalls()[18]);
+    REQUIRE_FALSE(player.IsStrokeLegal(table));
   }
 
   SECTION("Pocketing the cue ball is illegal.") {
-
+    player.EndStroke(true);
+    table.SetCueBallFirstContacted(ci::Color("brown"));
+    player.AddBallsPottedLastStroke(table.GetBalls().back());
+    REQUIRE_FALSE(player.IsStrokeLegal(table));
   }
 
   SECTION("First contacting a red ball is illegal.") {
-
+    player.EndStroke(true);
+    table.SetCueBallFirstContacted(ci::Color("red"));
+    REQUIRE_FALSE(player.IsStrokeLegal(table));
   }
 
   SECTION("First contacting any ball other than lowest point value color when "
       "no reds remain is illegal.") {
+    player.EndStroke(true);
+    for (size_t i = 0; i < 15; ++i) {
+      table.RemoveBallFromTable(table.GetBalls().front());
+    }
+    table.SetCueBallFirstContacted(ci::Color("pink"));
+    REQUIRE_FALSE(player.IsStrokeLegal(table));
+  }
 
+  SECTION("Pocketing any color is legal while reds are still on the table.") {
+    player.EndStroke(true);
+    table.SetCueBallFirstContacted(ci::Color("black"));
+    player.AddBallsPottedLastStroke(table.GetBalls()[20]);
+    REQUIRE(player.IsStrokeLegal(table));
+  }
+
+  SECTION("Must contact yellow first when no reds are on the table.") {
+    player.EndStroke(true);
+    for (size_t i = 0; i < 15; ++i) {
+      table.RemoveBallFromTable(table.GetBalls().front());
+    }
+    table.SetCueBallFirstContacted(ci::Color("yellow"));
+    REQUIRE(player.IsStrokeLegal(table));
+  }
+
+  SECTION("Must pocket pink when all lower point value Balls are pocketed.") {
+    player.EndStroke(true);
+    for (size_t i = 0; i < 19; ++i) {
+      table.RemoveBallFromTable(table.GetBalls().front());
+    }
+    table.SetCueBallFirstContacted(ci::Color("pink"));
+    player.AddBallsPottedLastStroke(table.GetBalls().front());
+    REQUIRE(player.IsStrokeLegal(table));
   }
 }
