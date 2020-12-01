e994fb91b58ea23c61dfd4f3839d5cab313ad841
rtpatel2 <rtpatel2@illinois.edu>
2020-12-01 18:52:06 -0500

    Created file to test functionality of GameEngine class.

---
+++ tests/game_engine_test.cc
@@ -0,0 +1,17 @@
+/**
+ * Created by Rishi Patel on 12/1/2020.
+ */
+
+#include "core/game_engine.h"
+#include "catch2/catch.hpp"
+
+using snooker::GameEngine;
+
+/**
+ * Testing strategy: test each function in the GameEngine class, including all
+ * relevant edge cases.
+ */
+
+TEST_CASE("Validate creating a GameEngine") {
+  
+}
