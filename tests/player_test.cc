3074faf82618094f7963e42f61caac2597c272f8
rtpatel2 <rtpatel2@illinois.edu>
2020-12-01 15:54:09 -0500

    Created test file for Player class.

---
+++ tests/player_test.cc
@@ -0,0 +1,20 @@
+/**
+ * Created by Rishi Patel on 12/1/2020.
+ */
+
+#include "core/player.h"
+#include "cinder/gl/gl.h"
+#include "catch2/catch.hpp"
+
+using snooker::Player;
+
+/**
+ * Testing strategy: test each function in the Player class, including all
+ * relevant edge cases.
+ */
+
+static const double kMarginOfError = 0.001;
+
+TEST_CASE("Creating a Player.") {
+  
+}
